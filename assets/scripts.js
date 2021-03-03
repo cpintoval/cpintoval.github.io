49260af9687e4bd46dc8a3e1844526f5ebe2dee7
Carlos Pinto <cpintovalderrama@gmail.com>
2021-03-03 13:26:39 -0500

    Adding console.log with link to source code

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,4 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
+  console.log("Looking for the source code? You can find it here: https://github.com/cpintoval/cpintoval.github.io");
 })
